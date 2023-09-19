904e8f4d3c298f920ccfd334e2f435fb5ff0cb12
fathima-n21 <fathimanazar212@gmail.com>
2023-09-19 11:45:37 +0530

    Initial commit

---
+++ CO2/Ques1.py
@@ -0,0 +1,10 @@
+#mean,median and standard deviation
+import numpy as np
+arr = np.array([11,16,34,89,45,23,40,29,72,66,83,57])
+print("Array:",arr)
+mean = np.mean(arr)
+print("Mean:",mean)
+median = np.median(arr)
+print("Median:",median)
+std_deviation = np.std(arr)
+print("Standard deviation:",std_deviation)
